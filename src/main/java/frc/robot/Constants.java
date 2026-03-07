f15f1cf8be78a8482410830e10b574c0561414d2
MatteoCiavaglia <dcrobotics@gmail.com>
2026-03-07 14:21:35 -0500

    Tag Transitions

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,6 +7,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.RobotBase;
 
 /**
@@ -28,4 +29,15 @@ public final class Constants {
     /** Replaying from a log file. */
     REPLAY
   }
+
+  ///for Tags 10, 2, 4, 5, 26, 18, 20, 21
+  private static final Translation2d TAG_SET_1 = new Translation2d(23.25, 0);
+  private static final int[] TAG_SET_1_IDS = {2,4,5,10,18,20,21,26};
+
+  ///for Tags 3, 11, 9, 19, 27, 25
+  private static final Translation2d TAG_SET_2 = new Translation2d(14.25, 23.25);
+  private static final int[] TAG_SET_2_IDS = {3,9,11,19,25,27};
+  /// for Tags 8, 24
+  private static final Translation2d TAG_SET_3 = new Translation2d(-14.25, 23.25);
+  private static final int[] TAG_SET_3_IDS = {8,24};
 }
