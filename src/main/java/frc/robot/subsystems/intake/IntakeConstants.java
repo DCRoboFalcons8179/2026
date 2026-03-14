5f3092f7b24daa4e9d467c78f98b170b459205d6
robofalcons8179 <dcrobotics@gmail.com>
2026-03-13 21:31:44 -0400

    new intake speeds

--- src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -10,8 +10,8 @@ public class IntakeConstants {
   public static final double GEAR_RATIO = 1.0 / 3.0;
 
   // motor velocity
-  public static final double SPEED_IN = -18.5;
-  public static final double SPEED_OUT = 18.5;
+  public static final double SPEED_IN = -27.5;
+  public static final double SPEED_OUT = 27.5;
 
   public static final double EXTRUDE_IN_SPEED = -6;
 
@@ -19,7 +19,7 @@ public class IntakeConstants {
   public static final NeutralModeValue NEUTRAL_MODE = NeutralModeValue.Coast;
 
   public static final CurrentLimitsConfigs CURRENT_LIMIT =
-      new CurrentLimitsConfigs().withSupplyCurrentLimit(30).withSupplyCurrentLimitEnable(true);
+      new CurrentLimitsConfigs().withSupplyCurrentLimit(40).withSupplyCurrentLimitEnable(true);
 
   public static final double KP = 0.2; // Proportional gain
   public static final double KI = 0.0; // Integral gain
